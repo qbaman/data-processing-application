78181b511530af5ac1d76138534797efba7e7ca9
qbaman <78668489+qbaman@users.noreply.github.com>
2026-01-30 10:35:55 +0000

    feat: wire DI for dataset repository and search services

--- FBZSystemMvc/Program.cs
+++ FBZSystemMvc/Program.cs
@@ -1,7 +1,30 @@
+using FBZ_System.Repositories;
+using FBZ_System.Services;
+using FBZ_System.Strategies;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IComicRepository>(sp =>
+{
+    var env = sp.GetRequiredService<IWebHostEnvironment>();
+    var dataPath = Path.Combine(env.ContentRootPath, "Data");
+    return new ComicRepositoryCsv(dataPath);
+});
+
+builder.Services.AddSingleton<IGroupingStrategy, GroupByAuthorStrategy>();
+builder.Services.AddSingleton<IGroupingStrategy, GroupByYearStrategy>();
+
+builder.Services.AddSingleton<ISortStrategy, SortTitleAscendingStrategy>();
+builder.Services.AddSingleton<ISortStrategy, SortTitleDescendingStrategy>();
+
+builder.Services.AddSingleton<ISearchService, SearchService>();
+builder.Services.AddSingleton<ComicFormatter>();
+builder.Services.AddSingleton<ISearchHistoryService, SearchHistoryService>();
+
 
 var app = builder.Build();
 
@@ -22,8 +45,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Dataset}/{action=Index}/{id?}");
 
 
 app.Run();
